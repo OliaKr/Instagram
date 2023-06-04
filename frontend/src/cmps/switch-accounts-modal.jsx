8ab78f813a37f31824a93f03f5e4bd434b5838ae
OliaK <olgamr@walla.com>
2023-06-04 18:53:42 +0300

    Improving the mobile view

--- frontend/src/cmps/switch-accounts-modal.jsx
+++ frontend/src/cmps/switch-accounts-modal.jsx
@@ -1,79 +1,86 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import Box from '@mui/material/Box'
-import Modal from '@mui/material/Modal'
-import vi from '../assets/icons/vi.svg'
-import Avatar from '@mui/material/Avatar'
-import { gUsers } from '../assets/services.js/user-service.js'
-import { switchUser, closeSwitchUsersModal } from '../store/user.action.js'
-import close from '../assets/icons/close.svg'
-import { useMediaQuery } from '@mui/material'
+import React from "react";
+import { useSelector } from "react-redux";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import vi from "../assets/icons/vi.svg";
+import Avatar from "@mui/material/Avatar";
+import { gUsers } from "../assets/services.js/user-service.js";
+import { switchUser, closeSwitchUsersModal } from "../store/user.action.js";
+import close from "../assets/icons/close.svg";
+import { useMediaQuery } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 export function SwitchAcoountsModal() {
-  const matches = useMediaQuery('(max-width:480px)')
+  const matches = useMediaQuery("(max-width:480px)");
   const isSwitchModalOpen = useSelector(
     (storeState) => storeState.userModule.isSwitchModalOpen
-  )
-  const user = useSelector((storeState) => storeState.userModule.user)
+  );
+  const user = useSelector((storeState) => storeState.userModule.user);
+  const navigate = useNavigate();
 
   function onLogin(userLine) {
-    switchUser(userLine)
-    closeSwitchUsersModal()
+    // console.log("userLine", userLine);
+    switchUser(userLine);
+    closeSwitchUsersModal();
+    // window.location.reload(false);
+    navigate(`/instagram/${userLine?.username}`, {
+      state: { otherUser: userLine },
+    });
   }
 
   const style = {
-    display: 'flex',
-    flexDirection: 'column',
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: matches ? '80%' : 370,
+    display: "flex",
+    flexDirection: "column",
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: matches ? "80%" : 370,
     // height: 370,
-    bgcolor: 'background.paper',
+    bgcolor: "background.paper",
     boxShadow: 24,
-    borderRadius: '2%',
-    overflowY: 'visible',
-  }
+    borderRadius: "2%",
+    overflowY: "visible",
+  };
 
   return (
     <Modal
       open={isSwitchModalOpen}
       onClose={closeSwitchUsersModal}
-      aria-labelledby='modal-modal-title'
-      aria-describedby='modal-modal-description'
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
     >
       <Box sx={style}>
-        <div className='switch-accounts-modal'>
-          <div className='modal-header'>
-            <span className='modal-title'>Switch accounts </span>
+        <div className="switch-accounts-modal">
+          <div className="modal-header">
+            <span className="modal-title">Switch accounts </span>
             <img
               src={close}
-              alt='close'
+              alt="close"
               onClick={closeSwitchUsersModal}
             />
           </div>
 
           {gUsers.map((userLine) => (
             <div
-              className='registered-user'
+              className="registered-user"
               key={userLine.id}
               onClick={() => onLogin(userLine)}
             >
-              <div className='left-section'>
+              <div className="left-section">
                 <Avatar
                   alt={userLine.fullname}
-                  sx={{ width: 24, height: 24, alignSelf: 'center' }}
+                  sx={{ width: 24, height: 24, alignSelf: "center" }}
                   src={userLine.imgUrl}
                 />
-                <span className='username'>{userLine.fullname}</span>
+                <span className="username">{userLine.fullname}</span>
               </div>
               {user && user.username === userLine.username ? (
                 <img
                   src={vi}
-                  width='30'
-                  height='30'
-                  alt='newPostImg'
+                  width="30"
+                  height="30"
+                  alt="newPostImg"
                 ></img>
               ) : null}
             </div>
@@ -81,5 +88,5 @@ export function SwitchAcoountsModal() {
         </div>
       </Box>
     </Modal>
-  )
+  );
 }
--- frontend/src/pages/messages-page.jsx
+++ frontend/src/pages/messages-page.jsx
@@ -1,150 +1,168 @@
-import React, { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
-import { UserMessage } from '../cmps/user-message'
-import pen from '../assets/icons/pen.svg'
-import roomIcon from '../assets/icons/roomIcon.svg'
-import phone from '../assets/icons/phone.svg'
-import video from '../assets/icons/video.svg'
-import info from '../assets/icons/info.svg'
-import { Avatar } from '@mui/material'
-import { ChatRoomFooter } from '../cmps/chat-room-footer'
-import { v4 as uuidv4 } from 'uuid'
-import io from 'socket.io-client'
-import { Chat } from '../cmps/chat'
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import { UserMessage } from "../cmps/user-message";
+import pen from "../assets/icons/pen.svg";
+import roomIcon from "../assets/icons/roomIcon.svg";
+import phone from "../assets/icons/phone.svg";
+import video from "../assets/icons/video.svg";
+import info from "../assets/icons/info.svg";
+import { Avatar, useMediaQuery } from "@mui/material";
+import { ChatRoomFooter } from "../cmps/chat-room-footer";
+import { v4 as uuidv4 } from "uuid";
+import io from "socket.io-client";
+import { Chat } from "../cmps/chat";
+import { ReactComponent as ArrowLeft } from "../assets/icons/arrow-left.svg";
+import { useNavigate } from "react-router-dom";
 
 function Messages() {
-  const socket = io.connect('')
-  const user = useSelector((storeState) => storeState.userModule.user)
-  const [userToMsg, setUserToMsg] = useState(null)
-  const [messageList, setMessageList] = useState([])
-  const [room, setRoom] = useState('')
+  const socket = io.connect("");
+  const user = useSelector((storeState) => storeState.userModule.user);
+  const [userToMsg, setUserToMsg] = useState(null);
+  const [messageList, setMessageList] = useState([]);
+  const [room, setRoom] = useState("");
+  const isMobile = useMediaQuery("(max-width:480px)");
+  const navigate = useNavigate();
 
   useEffect(() => {
-    socket.on('receive_message', (data) => {
-      setMessageList((list) => [...list, data])
-      console.log(messageList)
-    })
-  }, [messageList, socket])
+    socket.on("receive_message", (data) => {
+      setMessageList((list) => [...list, data]);
+      console.log(messageList);
+    });
+  }, [messageList, socket]);
 
   const joinRoom = (room) => {
     if (user && room) {
-      socket.emit('join_room', room)
-      socket.on('join_room', (data) => {
-        console.log(data)
-      })
+      socket.emit("join_room", room);
+      socket.on("join_room", (data) => {
+        console.log(data);
+      });
     }
-  }
+  };
 
   const showLastRoomMessage = () => {
     return user?.messages?.map((m) => {
-      const otherMsgs = m.list.filter((chat) => chat.fullname !== user.fullname)
+      const otherMsgs = m.list.filter(
+        (chat) => chat.fullname !== user.fullname
+      );
       return (
         <UserMessage
           key={uuidv4()}
           u={otherMsgs[otherMsgs.length - 1]}
           room={m?.room}
+          userToMsg={userToMsg}
           setUserToMsg={setUserToMsg}
           setRoom={setRoom}
           setMessageList={setMessageList}
           joinRoom={joinRoom}
         />
-      )
-    })
-  }
+      );
+    });
+  };
 
-  const onMessagesSearchModal = () => {}
+  const onMessagesSearchModal = () => {};
+
   return (
-    <div className='messages-container'>
-      <div className='messages-content'>
-        <div className='messages-list'>
-          <div className='messages-header'>
-            <div className='messages-header-left'>
-              <span>{user?.username}</span>
-              <span onClick={onMessagesSearchModal}>
-                <KeyboardArrowDownIcon fontSize='large' />
-              </span>
+    <div className="messages-container">
+      <div className="messages-content">
+        {isMobile && userToMsg ? null : (
+          <div className="messages-list">
+            <div className="messages-header">
+              <div className="messages-header-left">
+                {isMobile && (
+                  <ArrowLeft onClick={() => navigate("/instagram")} />
+                )}
+                <span>{user?.username}</span>
+                <span onClick={onMessagesSearchModal}>
+                  <KeyboardArrowDownIcon fontSize="large" />
+                </span>
+              </div>
+              <div className="messages-header-right">
+                <img
+                  src={pen}
+                  alt="penIcon"
+                />
+              </div>
             </div>
-            <div className='messages-header-right'>
-              <img
-                src={pen}
-                alt='penIcon'
-              />
+            <div className="messages-title">
+              <span>Messages</span>
+              <span>2 requests</span>
             </div>
+            <div className="messages">{showLastRoomMessage()}</div>
           </div>
-          <div className='messages-title'>
-            <span>Messages</span>
-            <span>2 requests</span>
-          </div>
-          <div className='messages'>{showLastRoomMessage()}</div>
-        </div>
+        )}
 
-        <div className='messages-room'>
-          {userToMsg ? (
-            <div className='chat-open'>
-              <div className='chat-open-header'>
-                <div className='chat-open-header-content'>
-                  <div className='chat-open-header-l'>
-                    <Avatar
-                      sx={{
-                        width: '24px',
-                        height: '24px',
-                      }}
-                      src={userToMsg?.avatar}
-                      alt='user'
-                    />
+        {isMobile && !userToMsg ? null : (
+          <div className="messages-room">
+            {userToMsg ? (
+              <div className="chat-open">
+                <div className="chat-open-header">
+                  <div className="chat-open-header-content">
+                    <div className="chat-open-header-l">
+                      {isMobile && (
+                        <ArrowLeft onClick={() => setUserToMsg(null)} />
+                      )}
+                      <Avatar
+                        sx={{
+                          width: "24px",
+                          height: "24px",
+                        }}
+                        src={userToMsg?.avatar}
+                        alt="user"
+                      />
 
-                    {userToMsg.fullname}
-                  </div>
-                  <div className='chat-open-header-r'>
-                    <img
-                      src={phone}
-                      alt='phoneIcon'
-                    />
-                    <img
-                      src={video}
-                      alt='videoIcon'
-                    />
-                    <img
-                      src={info}
-                      alt='infoIcon'
-                    />
+                      {userToMsg.fullname}
+                    </div>
+                    <div className="chat-open-header-r">
+                      <img
+                        src={phone}
+                        alt="phoneIcon"
+                      />
+                      <img
+                        src={video}
+                        alt="videoIcon"
+                      />
+                      <img
+                        src={info}
+                        alt="infoIcon"
+                      />
+                    </div>
                   </div>
                 </div>
+                <div className="chat-body">
+                  <Chat
+                    messageList={messageList}
+                    room={room}
+                  />
+                </div>
+                <div className="chat-footer">
+                  <ChatRoomFooter
+                    socket={socket}
+                    room={room}
+                    messageList={messageList}
+                    setMessageList={setMessageList}
+                  />
+                </div>
               </div>
-              <div className='chat-body'>
-                <Chat
-                  messageList={messageList}
-                  room={room}
+            ) : (
+              <div className="chat-closed">
+                <img
+                  src={roomIcon}
+                  alt="messagesIcon"
                 />
-              </div>
-              <div className='chat-footer'>
-                <ChatRoomFooter
-                  socket={socket}
-                  room={room}
-                  messageList={messageList}
-                  setMessageList={setMessageList}
-                />
-              </div>
-            </div>
-          ) : (
-            <div className='chat-closed'>
-              <img
-                src={roomIcon}
-                alt='messagesIcon'
-              />
 
-              <span>Your Messages</span>
-              <span>
-                Send private photos and messages to a friend or group.
-              </span>
-              <button>Send message</button>
-            </div>
-          )}
-        </div>
+                <span>Your Messages</span>
+                <span>
+                  Send private photos and messages to a friend or group.
+                </span>
+                <button>Send message</button>
+              </div>
+            )}
+          </div>
+        )}
       </div>
     </div>
-  )
+  );
 }
 
-export default Messages
+export default Messages;
